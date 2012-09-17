f67d4dbda711dc8f5d998aedb8a7beb82361b164
yusuke kurosawa <pkuroq@gmail.com>
2012-09-17 09:50:56 +0900

    コミット

---
+++ gen/com/sas_com/solution/pdt/locationchanger/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.sas_com.solution.pdt.locationchanger;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
