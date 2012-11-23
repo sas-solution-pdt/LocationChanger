cfd61567565759dd244474232bb50ed7a99a9ad5
yusuke kurosawa <pkuroq@gmail.com>
2012-11-23 17:42:57 +0900

    コミット

--- gen/com/sas_com/solution/pdt/locationchanger/R.java
+++ gen/com/sas_com/solution/pdt/locationchanger/R.java
@@ -11,12 +11,27 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int gps=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int kbluetoothd_32x32=0x7f020002;
+        public static final int kwifimanager_32x32=0x7f020003;
+        public static final int kwifimanager_64x64=0x7f020004;
     }
     public static final class id {
-        public static final int button1=0x7f050000;
-        public static final int button2=0x7f050001;
-        public static final int mapview=0x7f050002;
+        public static final int ImageView01=0x7f050005;
+        public static final int ImageView02=0x7f050008;
+        public static final int ImageView03=0x7f05000b;
+        public static final int TextView01=0x7f050006;
+        public static final int TextView02=0x7f050009;
+        public static final int TextView03=0x7f05000c;
+        public static final int button01=0x7f050000;
+        public static final int button02=0x7f050001;
+        public static final int button03=0x7f050002;
+        public static final int button1=0x7f050003;
+        public static final int mapview=0x7f050004;
+        public static final int toggleButton01=0x7f050007;
+        public static final int toggleButton02=0x7f05000a;
+        public static final int toggleButton03=0x7f05000d;
     }
     public static final class layout {
         public static final int locationdetail=0x7f030000;
